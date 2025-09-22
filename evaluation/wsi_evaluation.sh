350e94b59cc17e721c348ae65c6351037d9c9aba
Hyeseong Lee <gotjd709@korea.ac.kr>
2025-09-22 22:20:58 +0900

    Delete evaluation/wsi_evaluation.sh

--- evaluation/wsi_evaluation.sh
+++
@@ -1,6 +0,0 @@
-python wsi_evaluation.py \
-    -m resnet50 \
-    -mp /workspace/gist/script/stomach-subtype/test/resnet50_base.pth \
-    -icp /workspace/gist/data/patch/resnet50_base_internal_test_feature.csv \
-    -ecp /workspace/gist/data/patch/resnet50_base_external_test_feature.csv \
-    -rsp ./result_save_path.txt
