2a9781659199cde2557552a5256e4e0980dceb0f
Hyeseong Lee <gotjd709@korea.ac.kr>
2025-09-22 22:20:26 +0900

    Delete evaluation/feature_extract.sh

--- evaluation/feature_extract.sh
+++
@@ -1,7 +0,0 @@
-python feature_extract.py \
-    -m resnet50 \
-    -msp /workspace/gist/script/stomach-subtype/test/resnet50_base.pth \
-    -tbd /workspace/gist/data/patch/ \
-    -tpd /workspace/gist/data/patch/internal_test/ \
-    -trp /workspace/gist/script/stomach-subtype/data/train_zip.pickle \
-    -vp /workspace/gist/script/stomach-subtype/data/valid_zip.pickle \
